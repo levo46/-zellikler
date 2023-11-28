b3980c819f8d89943e226a2f725bcb45f31360b7
levent581 <levent581@BPC03>
2023-11-28 09:26:04 +0300

    Proje dosyası ekle.

---
+++ Özellikler/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Özellikler
+{
+    public class Ucgen 
+    {
+        int a=5;
+        int b=4;
+        int c=3;
+        public int A 
+        { 
+            get { return a; }
+            set 
+            {
+                if ( value<=0) 
+                {
+                    Console.WriteLine("hatalı sayı");
+                   
+                }
+                else
+                    a = value;
+            }
+
+        }
+        public int B
+        {
+            get { return b; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("hatalı sayı");
+
+                }
+                else
+                    b = value;
+            }
+
+        }
+        public int C
+        {
+            get { return c; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("hatalı sayı");
+
+                }
+                else
+                    c = value;
+            }
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
